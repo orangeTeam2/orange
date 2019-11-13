f2c77296a6c7cd92de4bccb13f92e398a4d94d2d
59536 <595364518@qq.com>
2019-11-13 19:11:00 +0800

    ver1.0

---
+++ src/main/java/com/lhb/springboot/SpringBootLhbOrangeApplication.java
@@ -0,0 +1,13 @@
+package com.lhb.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLhbOrangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLhbOrangeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lhb/springboot/SpringBootLhbOrangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhb.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLhbOrangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
